f776155d5ee16674432403c43a70c2bb9b05d428
Mochizuki Ayumu <b1404928@planet.kanazawa-it.ac.jp>
2014-11-25 16:28:41 +0900

    Revert "とりあえず"
    This reverts commit d50c2a1dd8dbde1633d24c2b8f7eb0774742ef16.

--- ARPreVer/src/jp/kanazawaapp/kit/ARPreviewActivity.java
+++ ARPreVer/src/jp/kanazawaapp/kit/ARPreviewActivity.java
@@ -1,22 +1,13 @@
 package jp.kanazawaapp.kit;
 
-import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.hardware.GeomagneticField;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,9 +15,7 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.maps.GeoPoint;
-
-public class ARPreviewActivity extends Activity implements SensorEventListener,LocationListener{
+public class ARPreviewActivity extends Activity implements SensorEventListener{
 
 	//ARview
 	private ArView arView;
@@ -37,25 +26,6 @@ public class ARPreviewActivity extends Activity implements SensorEventListener,L
 	private float[] accelerometerValues = new float[3];
 	//x,y,z軸の磁気密度
 	private float[] magneticValues =new float[3];
-	
-//location関係
-    private LocationManager locationManager;
-    //グーグルマップからの位置取得
-    private GeoPoint geoPoint;
-    //磁北補正用
-    private GeomagneticField geomagneticField;  
-// データベースで使用する変数
-    //データベース名
-    private final static String DB_NAME = "gps_data.db";
-    //使用するテーブル
-    private final static String DB_TABLE = "gps_data";
-    //バージョン情報
-    private final static int DB_VERSION = 1;
-    //コンストラクタ
-    private SQLiteDatabase db;
-    //カーソル
-	private Cursor cursor;
-    
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,13 +37,10 @@ public class ARPreviewActivity extends Activity implements SensorEventListener,L
         //タイトルバーの表示を解除
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
-        // データベースの用意
-        initData();
+		
         
      // ARViewの取得
-        arView = new ArView(this,cursor);
-        //閉じる
-        cursor.close();
+        arView = new ArView(this);
         
         //各種センサーの用意
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -86,41 +53,8 @@ public class ARPreviewActivity extends Activity implements SensorEventListener,L
         setContentView(new CameraView(this));
         addContentView(arView, new LayoutParams(LayoutParams.MATCH_PARENT,
                 LayoutParams.MATCH_PARENT));
-        
-        
 
 	}
-	//activity実行直前
-		@Override
-		protected void onResume() {
-			super.onResume();
-	        		
-			 //ロケーションマネージャの設定
-	        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, this);
-	        		
-			//センサー処理の登録
-			
-			/* sensorManager
-			 * 第1引数		センサーによって呼び出される関数を持つクラス
-			 * 第2引数		対象となる値を
-			 *　第3引数		呼び出し頻度   (ゲームにするなら SENSOR_DELAY_GAME)
-			 * */
-	        sensorManager.registerListener(this,
-	                listMag.get(0), SensorManager.SENSOR_DELAY_NORMAL);
-	        sensorManager.registerListener(this,
-	                listAcc.get(0), SensorManager.SENSOR_DELAY_NORMAL);
-		}
-		
-		@Override
-		protected void onStop() {
-			super.onStop();
-			//位置情報の取得の停止
-			locationManager.removeUpdates(this);
-			//センサーを終了
-			sensorManager.unregisterListener(this);
-
-		}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -168,104 +102,46 @@ public class ARPreviewActivity extends Activity implements SensorEventListener,L
             
             
             // 求まった方位角をラジアンから度に変換する
-            float direction = (float) Math.toDegrees(actual_orientation[0])
-            		//偏差を加算
-                    + geomagneticField.getDeclination();
-           //ArViewに値を渡す
+            float direction = (float) Math.toDegrees(actual_orientation[0]);
+
+            
           //描画をする
-            arView.drawScreen(direction,geoPoint);
+            arView.drawScreen(direction);
         }
     }
 
-	
-	/*LocationLisner*/
+		
+
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
+	
+	//activity実行直前
 	@Override
-	public void onLocationChanged(Location location) {
-		   geoPoint = new GeoPoint((int) (location.getLatitude() * 1E6),
-	                (int) (location.getLongitude() * 1E6));
-		   
-	        geomagneticField = new GeomagneticField((float) location.getLatitude(),
-	                (float) location.getLongitude(), (float) location.getAltitude(),
-	                new Date().getTime());
+	protected void onResume() {
+		super.onResume();
+        		
+        		
+		//センサー処理の登録
 		
-	}
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-	@Override
-	public void onProviderEnabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-	@Override
-	public void onProviderDisabled(String provider) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		/* sensorManager
+		 * 第1引数		センサーによって呼び出される関数を持つクラス
+		 * 第2引数		対象となる値を
+		 *　第3引数		呼び出し頻度   (ゲームにするなら SENSOR_DELAY_GAME)
+		 * */
+        sensorManager.registerListener(this,
+                listMag.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,
+                listAcc.get(0), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 	
-	//データベース系
-	public void initData() {
-        // SQLiteOpenHelperを継承したクラスを使用してデータベースを作成します
-        SQLiteOpenHelperEx helper = new SQLiteOpenHelperEx(this);
-        db = helper.getWritableDatabase();
+	@Override
+	protected void onStop() {
+		super.onStop();
+		//センサーを終了
+		sensorManager.unregisterListener(this);
 
-        cursor = db.query(DB_TABLE, new String[] { "info", "latitude",
-                "longitude" }, null, null, null, null, null);
-        // テーブルが空の時内容をセットする
-        if (cursor.getCount() < 1) {
-            presetTable();
-            cursor = db.query(DB_TABLE, new String[] { "info", "latitude",
-                    "longitude" }, null, null, null, null, null);
-        }
-    }
-	private void presetTable() {
-        // テーブルの内容が空の時以下の内容をセットする
-        ContentValues values = new ContentValues();
-        values.put("info","金沢工業大学");
-        values.put("latitude",36530349);
-        values.put("longitude", 136627751);
-        db.insert(DB_TABLE, "", values);
-    }
-	
-	
-	
-	
-	
-	//データベース用のクラス
-	 public class SQLiteOpenHelperEx extends SQLiteOpenHelper {
-	        // コンストラクタ
-	        public SQLiteOpenHelperEx(Context context) {
-	        	//データベースの生成 
-	        	//2つ目の引数をnullにすることでメモリ上にデータベースが作られる
-	            super(context, DB_NAME, null, DB_VERSION);
-	        }
-
-	        @Override
-	        public void onCreate(SQLiteDatabase db) {
-	            // テーブルの作成
-	        	//実行したいSQL文を格納
-	            String sql = "create table if not exists "
-	                    + DB_TABLE
-	                    + "(info text, latitude numeric, longitude numeric)";
-	            db.execSQL(sql);
-
-	        }
-
-	        @Override
-	        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-	            // データベースのアップグレード
-	            // ここでは、テーブルを作り直しをしています
-	            db.execSQL("drop table if exists " + DB_TABLE);
-	            onCreate(db);
-	        }
-	        
-	    }
-	 
+	}
 
 	
 }
--- ARPreVer/src/jp/kanazawaapp/kit/ArView.java
+++ ARPreVer/src/jp/kanazawaapp/kit/ArView.java
@@ -1,18 +1,11 @@
 package jp.kanazawaapp.kit;
 
-import java.util.ArrayList;
-
 import android.content.Context;
-import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Paint.FontMetrics;
 import android.graphics.Path;
-import android.graphics.RectF;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 
 import com.google.android.maps.GeoPoint;
 
@@ -26,34 +19,8 @@ public class ArView extends View {
     // 向きを保持する変数 (方角を指定する)
     float direction;
     
-    // 現在地を保持する変数
-    int nowLocationX, nowLocationY;
-    
-    // ARテキストの情報を保持するオブジェクト
- 	private ArrayList<GPSData> gpsDataList;
- 	
-	// カメラの画角を指定する 後にAPIで画角を取得し動的指定
-	private final int ANGLE = 60;
-	// ARテキストの見える範囲を指定する(メートル表記)
-	private final float VIEW_LIMIT = 100000;
-	// ディスプレイサイズ
-	private int displayX;
-	
-	//地球の半径
-	public static final double EARTH = 6378.137;
-
- 	
-    
-	public ArView(Context context,Cursor cursor) {
+	public ArView(Context context) {
 		super(context);
-		//データベースの読み込み
-		readTable(cursor);
-		
-		// 画面サイズの取得
-				Display disp = ((WindowManager) context
-						.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-				//getSizeに変更予定
-				displayX = disp.getWidth();
 	}
 
 	@Override
@@ -63,98 +30,11 @@ public class ArView extends View {
 		//アンチエイリアス処理　= 曲線や斜めのギザギザを少なくすること
 		paint.setAntiAlias(true);
 		
-		//方角を示すためのコンパス描画開始 現在は三角形だが、きちんと作る　画像？パス？要選択
-				drawCompass(canvas, paint);
-		
-		//ARテキストの描画
-				for (int i = 0; i < gpsDataList.size(); i++) {
-					// データの読み込み
-					GPSData data = gpsDataList.get(i);
-					String info = data.info;
-					int y = data.latitude;
-					int x = data.longitude;
-
-					// ARテキストとの距離を求め、ラジアンに変換する
-					double dx = Math.toRadians(x - nowLocationX);
-					double dy = Math.toRadians(y - nowLocationY);
-					
-					float distance = (float) Math.sqrt(Math.pow(EARTH * dx,2) +
-							Math.pow(Math.cos(Math.toRadians(nowLocationX)) * EARTH * dy, 2));
-					
-
-					// ARテキストとの距離が一定以上離れていたら、処理を行わずに次のARテキストの処理を行う
-					if (distance > VIEW_LIMIT) {
-						continue;
-					}
-					// ARテキストと現在地のなす角を求めて正規化する
-					double angle = Math.atan2(y - nowLocationY, x - nowLocationX);
-					//度に変換
-					float degree = (float) Math.toDegrees(angle);
-					degree = -degree + 90;
-					if (degree < 0)
-						degree = 360 + degree;
-
-					// 端末の向きとARテキストとの角度の差を求める
-					float sub = degree - direction;
-					if (sub < -180.0)
-						sub += 360;
-					if (sub > 180.0)
-						sub -= 360;
-
-					// ARテキストが視野に存在すれば描画処理を行う
-					if (Math.abs(sub) < (ANGLE / 2)) {
-						// 距離によってARテキストのサイズを決める
-						float textSize = 50 * (float) (VIEW_LIMIT - distance)/ VIEW_LIMIT;
-						paint.setTextSize(textSize);
-
-						// ARテキストの描画を描画する
-						float textWidth = paint.measureText(info);
-						float diff = (sub / (ANGLE / 2)) / 2;
-						float left = (displayX / 2 + displayX * diff) - (textWidth / 2);
-						drawBalloonText(canvas, paint, info, left, 55);
-
-					}
-				}
-
-		
-		
-			}
-	
-	private void drawBalloonText(Canvas canvas, Paint paint, String text,float left, int top) {
-		// 文字列の幅を取得
-				float textWidth = paint.measureText(text);
-				// フォント情報の取得
-				FontMetrics fontMetrics = paint.getFontMetrics();
-
-				// 文字列の5ポイント外側を囲む座標を求める
-				float bLeft = left - 5;
-				float bRight = left + textWidth + 5;
-				float bTop = top + fontMetrics.ascent - 5;
-				float bBottom = top + fontMetrics.descent + 5;
-
-				// 吹き出しの描画
-				RectF rectF = new RectF(bLeft, bTop, bRight, bBottom);
-				paint.setColor(Color.LTGRAY);
-				canvas.drawRoundRect(rectF, 5, 5, paint);
-
-				// 三角形の描画
-				paint.setStyle(Paint.Style.FILL_AND_STROKE);
-				Path path = new Path();
-				float center = left + textWidth / 2;
-				float triangleSize = paint.getTextSize() / 3;
-				path.moveTo(center, bBottom + triangleSize);
-				path.lineTo(center - triangleSize / 2, bBottom - 1);
-				path.lineTo(center + triangleSize / 2, bBottom - 1);
-				path.lineTo(center, bBottom + triangleSize);
-				canvas.drawPath(path, paint);
-
-				// 文字列の描画
-				paint.setColor(Color.WHITE);
-				canvas.drawText(text, left, top, paint);
-		
+		//描画開始 複数出きるようにするため今後はfor文か何かでループ予定　紹介する場所は　ArrayListか？
+		drawObject(canvas, paint);
 	}
-
-	private void drawCompass(Canvas canvas, Paint paint) {
+	
+	private void drawObject(Canvas canvas, Paint paint) {
 		 Path path = new Path();
 		 path.moveTo(POS_COMPASSX, POS_COMPASSY - 20);
 		 path.lineTo(POS_COMPASSX + 10, POS_COMPASSY + 10);
@@ -167,41 +47,10 @@ public class ArView extends View {
 	}
 	
 	//センサー値の取得と再描画
-    public void drawScreen(float preDirection,GeoPoint geoPoint) {
+    public void drawScreen(float preDirection) {
     	// センサーの値から端末の向きを計算する
         direction = (preDirection + 450) % 360;
-        if(geoPoint != null){
-            nowLocationX = geoPoint.getLatitudeE6();
-            nowLocationY = geoPoint.getLongitudeE6();
-        }
         // onDrawを呼び出して再描画
         invalidate();
     }
-    
-    
-    public void readTable(Cursor cursor) {
-		// データベースに保存されている
-		// 全てのARテキストの情報をlistに読み込む
-		if (gpsDataList != null)
-			gpsDataList.clear();
-		gpsDataList = new ArrayList();
-
-		cursor.moveToFirst();
-		do {
-			GPSData data = new GPSData();
-			data.info = cursor.getString(0);
-			data.latitude = cursor.getInt(1);
-			data.longitude = cursor.getInt(2);
-			gpsDataList.add(data);
-		} while (cursor.moveToNext());
-		//なくなるまでリストに追加する
-	}
-    
-    
-    //GPS情報を保持するクラス
-    class GPSData {
-		public String info;	
-		public int latitude;	//緯度
-		public int longitude;	//経度
-	}
 }
