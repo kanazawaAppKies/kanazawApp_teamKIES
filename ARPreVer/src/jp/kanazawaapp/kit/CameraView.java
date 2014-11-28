2dd05d62feb4b3b32ad148dd5c4a0d1deeac7084
Mochizuki Ayumu <b1404928@planet.kanazawa-it.ac.jp>
2014-11-29 00:21:00 +0900

    強豪の修正？

--- ARPreVer/src/jp/kanazawaapp/kit/CameraView.java
+++ ARPreVer/src/jp/kanazawaapp/kit/CameraView.java
@@ -1,6 +1,7 @@
 package jp.kanazawaapp.kit;
 
 import java.util.List;
+
 import android.content.Context;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
@@ -15,8 +16,8 @@ public class CameraView extends SurfaceView  implements SurfaceHolder.Callback{
 	public CameraView(Context context) {
 		super(context);
 		 //サーフェイスホルダーの取得とコールバック通知先の指定
-		holder = getHolder();
-		holder.addCallback(this);
+        holder = getHolder();
+        holder.addCallback(this);
 //      SDK3.1以降不要ためコメントアウト
 //   	  // SurfaceViewの種別をプッシュバッファーに変更します 
 //        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -27,10 +28,10 @@ public class CameraView extends SurfaceView  implements SurfaceHolder.Callback{
 	public void surfaceCreated(SurfaceHolder holder) {
 		//カメラが開けないなら何もしない
 		try {
-			camera = Camera.open();
-			camera.setPreviewDisplay(holder);
-		} catch (Exception e) {
-		}
+            camera = Camera.open();
+            camera.setPreviewDisplay(holder);
+        } catch (Exception e) {
+        }
 		
 	}
 
@@ -58,7 +59,7 @@ public class CameraView extends SurfaceView  implements SurfaceHolder.Callback{
 		camera.setParameters(parameters);
 
 		// プレビューの開始
-		camera.startPreview();
+        camera.startPreview();
 		
 	}
 
@@ -66,9 +67,9 @@ public class CameraView extends SurfaceView  implements SurfaceHolder.Callback{
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		camera.setPreviewCallback(null);
-		camera.stopPreview();
-		camera.release();
-		camera = null;
+        camera.stopPreview();
+        camera.release();
+        camera = null;
 		
 	}
 
