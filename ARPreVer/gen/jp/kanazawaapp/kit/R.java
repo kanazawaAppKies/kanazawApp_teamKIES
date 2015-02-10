fb78872459baa00c9d41b2bf8ac486bc1fdfe0b6
Mochizuki Ayumu <b1404928@planet.kanazawa-it.ac.jp>
2015-02-10 20:17:19 +0900

    no message

--- ARPreVer/gen/jp/kanazawaapp/kit/R.java
+++ ARPreVer/gen/jp/kanazawaapp/kit/R.java
@@ -1105,7 +1105,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1150,6 +1150,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textInfo=0x7f05003e;
         public static final int textView1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
